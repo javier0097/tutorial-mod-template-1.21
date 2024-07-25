4f1de97a7d9761cfa7d5e15456b15673f8208daf
Javier Martinez <javier0097@hotmail.com>
2024-07-25 17:05:20 -0400

    registro de items y grupo de items

--- src/main/java/net/javier/tutorialmod/TutorialMod.java
+++ src/main/java/net/javier/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.javier.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.javier.tutorialmod.item.ModItemGroups;
+import net.javier.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }

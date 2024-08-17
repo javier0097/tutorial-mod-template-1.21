a128643fbf502dc0913f6d1e5aede45cb3ff3f65
Javier Martinez <javier0097@hotmail.com>
2024-08-17 13:03:32 -0400

    tomate añadido

--- src/main/java/net/javier/tutorialmod/item/ModItems.java
+++ src/main/java/net/javier/tutorialmod/item/ModItems.java
@@ -15,6 +15,7 @@ public class ModItems{
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new Item.Settings()));
     public static final Item METAL_DETECTOR = registerItem("metal_detector",
             new MetalDetectorItem(new Item.Settings().maxDamage(64)));
+    public static final Item TOMATO = registerItem("tomato", new Item(new Item.Settings().food(ModFoodComponents.TOMATO)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);

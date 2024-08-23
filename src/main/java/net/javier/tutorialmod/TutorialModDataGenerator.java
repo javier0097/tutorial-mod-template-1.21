e97ddcf67589f9716fc31b2042d0e257caa62d9f
Javier Martinez <javier0097@hotmail.com>
2024-08-23 14:41:10 -0400

    datagem aplicado

--- src/main/java/net/javier/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/javier/tutorialmod/TutorialModDataGenerator.java
@@ -2,10 +2,21 @@ package net.javier.tutorialmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.javier.tutorialmod.datagen.ModBlockTagProvider;
+import net.javier.tutorialmod.datagen.ModItemTagProvider;
+import net.javier.tutorialmod.datagen.ModLootTableProvider;
+import net.javier.tutorialmod.datagen.ModModelProvider;
+import net.javier.tutorialmod.datagen.ModRecipeProvider;
 
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/net/javier/tutorialmod/datagen/ModLootTableProvider.java
@@ -0,0 +1,51 @@
+package net.javier.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.javier.tutorialmod.block.ModBlocks;
+import net.javier.tutorialmod.item.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+
+    public ModLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(ModBlocks.RUBY_BLOCK);
+        addDrop(ModBlocks.RAW_RUBY_BLOCK);
+        addDrop(ModBlocks.SOUND_BLOCK);
+
+        addDrop(ModBlocks.RUBY_ORE, copperLikeOreDrops(ModBlocks.RUBY_ORE, ModItems.RAW_RUBY));
+        addDrop(ModBlocks.DEEPSLATE_RUBY_ORE, copperLikeOreDrops(ModBlocks.DEEPSLATE_RUBY_ORE, ModItems.RAW_RUBY));
+        addDrop(ModBlocks.END_STONE_RUBY_ORE, copperLikeOreDrops(ModBlocks.END_STONE_RUBY_ORE, ModItems.RAW_RUBY));
+    }
+
+    public LootTable.Builder copperLikeOreDrops(Block drop, Item item) {
+        RegistryWrapper.Impl<Enchantment> impl = this.registryLookup.getWrapperOrThrow(RegistryKeys.ENCHANTMENT);
+        return this.dropsWithSilkTouch(
+                drop,
+                (LootPoolEntry.Builder<?>)this.applyExplosionDecay(
+                        drop,
+                        ItemEntry.builder(item)
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0F, 5.0F)))
+                                .apply(ApplyBonusLootFunction.oreDrops(impl.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
+    }
+}
